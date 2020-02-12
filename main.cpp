5ac231d9046136b8c7880f42943bdd725543ca38
Sluzhynskyi <sluzhynskyi@gmail.com>
2020-02-12 17:52:50 +0200

    Reading from term

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include<string>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char** argv)
+{
+    int method_ind = std::stoi(argv[1]);
+    string file_in = argv[2];
+    string file_out = argv[3];
+
     return 0;
 }
