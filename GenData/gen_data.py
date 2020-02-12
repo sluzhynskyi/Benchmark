a1d5e00cfe82b1dbc99c43f30eb43fa2dc98ec4b
Sluzhynskyi <sluzhynskyi@gmail.com>
2020-02-12 16:57:15 +0200

    Fixed fetch problem

--- GenData/gen_data.py
+++ GenData/gen_data.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 import string
 
@@ -7,13 +6,3 @@ chars = ''.join([random.choice(string.printable) for i in range(n)])
 with open('infile.txt', 'w+') as f:
     f.write(chars)
 
-=======
-import random
-import string
-
-n = 1024 ** 2  # 1 Mb of text
-chars = ''.join([random.choice(string.printable) for i in range(n)])
-with open('infile.txt', 'w+') as f:
-    f.write(chars)
-
->>>>>>> aca3b45a8b028412603c42c4644e750b6ccfe1d3
