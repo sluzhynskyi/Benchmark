273664028c5c5fc4c71269a3c62097ff64a75807
ostapdyg <dyhdalovych@ucu.edu.ua>
2020-02-18 18:33:29 +0200

    Working script

--- script.py
+++ script.py
@@ -3,29 +3,55 @@ import string
 import argparse
 import sys
 import subprocess
+from collections import defaultdict
+from math import inf
 # from functools import reduce
 NUM_METHODS = 3
 
-def count_symbols(filename:str)->int:
+
+def count_symbols(filename: str) -> int:
     with open(filename, "r") as f:
-        
+
         return sum((0 if i in string.whitespace else 1 for i in f.read()))
         # return reduce(lambda y, z: y + (0 if z in string.whitespace else 1), f.read(), 0)
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Benchmarking a C++ program")
-    parser.add_argument("num_runs", help="Number of function calls for each type", type=int)
+    parser.add_argument(
+        "num_runs", help="Number of function calls for each type", type=int)
     parser.add_argument("in_file", help="Input file path")
     parser.add_argument("out_file", help="Input file path")
     args = parser.parse_args(sys.argv[1:])
-    # //print(args.num_runs)
+
     try:
         open(args.in_file).close()
     except FileNotFoundError:
         print("Input file not found!")
-    
-    for method in range(NUM_METHODS):
-        with subprocess.Popen(f"build/Benchmark.exe {method+1} {args.in_file} {args.out_file}", stdout=subprocess.PIPE) as prog:
-            output = prog.stdout.read()
-            print(output)
+    # Create or clear output file
+    open(args.out_file, "w").close()
+
+    best_times = defaultdict(lambda: inf)
+    print(f"{'Reading file, using:':30}Best time(us):")
+
+    for method_id in range(NUM_METHODS):
+        for _ in range(args.num_runs):
+
+            with subprocess.Popen(f"build/Benchmark.exe {method_id+1} {args.in_file} {args.out_file}", stdout=subprocess.PIPE) as prog:
+                output = prog.stdout.read().decode("utf_8")
+                method_time = int(output.split("\n")[-1])
+                best_times[method_id] = min(best_times[method_id], method_time)
+            method_name = output.split("using ")[1].split(
+                " (microseconds)")[0]+":"
+        print(f"{method_name:30}{method_time}")
+
+    num_symbols = count_symbols(args.in_file)
+    errors = 0
+
+    with open(args.out_file) as f:
+        for line in f.readlines():
+            if(int(line.split(": ")[-1]) != num_symbols):
+                errors += 1
+
+    print("All results are the same " if(errors == 0)
+          else f"Errors encountered:{errors}")
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include<string>
-#include<sstream>
+#include <string>
+#include <sstream>
 #include <fstream>
 #include "functions.h"
 
@@ -18,22 +18,24 @@ inline std::chrono::high_resolution_clock::time_point get_current_time_fenced()
     return res_time;
 }
 
-template<class D>
-inline long long to_us(const D& d)
+template <class D>
+inline long long to_us(const D &d)
 {
     return std::chrono::duration_cast<std::chrono::microseconds>(d).count();
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // Reading variables from terminal
     int method_ind = std::stoi(argv[1]);
     string file_in = argv[2];
     string file_out = argv[3];
 
     // Reading file
-    cout << file_in.c_str() << endl;
+    // cout << file_in.c_str() << endl;
     std::ifstream in(file_in.c_str());
-    if (!in) {
+    if (!in)
+    {
         cerr << "Error opening file" << endl;
         return 1;
     }
@@ -42,41 +44,45 @@ int main(int argc, char **argv) {
     std::string s;
     auto starting_time = get_current_time_fenced();
     // switch/case doesn't take much time
-    switch(method_ind){
-        case 1:
-            method_description = "ss<<in.rdbuf()";
-            s = best(in);
-            break;
-        case 2:
-            method_description = "istreambuf_iterator{in}";
-            s = using_iterators(in);
-            break;
-        case 3:
-            method_description = "For large files method";
-            s = for_large_files(in);
-            break;
-        default:
-            std::cout<<"invalid method name!"<<endl;
+    switch (method_ind)
+    {
+    case 1:
+        method_description = "s << in.rdbuf()";
+        s = best(in);
+        break;
+    case 2:
+        method_description = "istreambuf_iterator{in}";
+        s = using_iterators(in);
+        break;
+    case 3:
+        method_description = "in.read(s[0], s.size())";
+        s = for_large_files(in);
+        break;
+    default:
+        std::cout << "invalid method name!" << std::endl;
     }
     auto duration = get_current_time_fenced() - starting_time;
 
     int symbols = 0;
-    for(auto ch:s){
-        if(!std::isspace(ch)){
-            symbols+=1;
+    for (auto ch : s)
+    {
+        if (!std::isspace(ch))
+        {
+            symbols += 1;
         }
     }
-//    std::ofstream out;
-//    out.open(file_out, ios::out);
-    std::ofstream out(file_out);
-    if (!out) {
+    //    std::ofstream out;
+    //    out.open(file_out, ios::out);
+    std::ofstream out(file_out, std::ios::app);
+    if (!out)
+    {
         cerr << "Error opening file" << endl;
         return 1;
     }
-    out<<"\""<<method_description<<"\": "<<symbols;
+    out << method_description << ": " << symbols << std::endl;
 
-    std::cout<<"Reading file using"<<method_description<<"(microseconds):"<<std::endl<<to_us(duration);
+    std::cout << "Reading file using " << method_description << " (microseconds):" << std::endl
+              << to_us(duration);
 
     return 0;
-
 }
