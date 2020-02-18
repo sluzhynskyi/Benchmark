9740c68ba22dc6696ad4627e6e852700a4096a36
ostapdyg <dyhdalovych@ucu.edu.ua>
2020-02-18 14:32:39 +0200

    upd

--- script.py
+++ script.py
@@ -1,11 +1,30 @@
 
 import string
+import argparse
+import sys
+import subprocess
+# from functools import reduce
+NUM_METHODS = 3
 
 def count_symbols(filename:str)->int:
     with open(filename, "r") as f:
+        
         return sum((0 if i in string.whitespace else 1 for i in f.read()))
-
-
+        # return reduce(lambda y, z: y + (0 if z in string.whitespace else 1), f.read(), 0)
+    
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser(description="Benchmarking a C++ program")
+    parser.add_argument("num_runs", help="Number of function calls for each type", type=int)
+    parser.add_argument("in_file", help="Input file path")
+    parser.add_argument("out_file", help="Input file path")
+    args = parser.parse_args(sys.argv[1:])
+    # //print(args.num_runs)
+    try:
+        open(args.in_file).close()
+    except FileNotFoundError:
+        print("Input file not found!")
+    
+    for method in range(NUM_METHODS):
+        with subprocess.Popen(f"build/Benchmark.exe {args.num_runs} {args.in_file} {args.out_file}", stdin=subprocess.PIPE) as prog:
+            print(prog.stdin.read().split())
